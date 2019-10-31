6cb5ce89a25d0edc6bac64db495ee85ed403b74c
haitao <810978626@qq.com>
2019-10-31 14:22:18 +0800

    #加入路由

--- routers/router.go
+++ routers/router.go
@@ -10,5 +10,17 @@ func init() {
 	beego.Router("/admin/manage/index", &admin.ManageController{}, "Get:Index")
 	beego.Router("/admin/manage/cache", &admin.ManageController{}, "Get:ReloadCache")
 
+	beego.Router("/admin/resource/index", &admin.ResourceController{}, "*:Index")
+	beego.Router("/admin/resource/treeGrid", &admin.ResourceController{}, "POST:TreeGrid")
+	beego.Router("/admin/resource/edit/?:id", &admin.ResourceController{}, "Get,Post:Edit")
+	beego.Router("/admin/resource/parent", &admin.ResourceController{}, "Post:ParentTreeGrid")
+	beego.Router("/admin/resource/delete", &admin.ResourceController{}, "Post:Delete")
+
+	beego.Router("admin/resource/select", &admin.ResourceController{}, "Get:Select")
+	beego.Router("admin/resource/chooseIcon", &admin.ResourceController{}, "Get:ChooseIcon")
+
+	beego.Router("/admin/resource/usermenutree", &admin.ResourceController{}, "POST:UserMenuTree")
+	beego.Router("/admin/resource/checkUrlFor", &admin.ResourceController{}, "POST:CheckUrlFor")
+
 	beego.Router("/", &admin.ManageController{}, "*:Index")
 }
